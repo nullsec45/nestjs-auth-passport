f4f0af8893cea6d12641009fc1e9282e74e0c162
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-11-05 23:53:31 +0700

    import jwt service to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
+import { JwtService } from '@nestjs/jwt/dist';
 
 @Module({
   imports: [UsersModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService, AuthService],
+  providers: [AppService, AuthService, JwtService],
 })
-export class AppModule {}
+export class AppModule { }
