794f418cefb7d2c1c4dd6db909337f6f67becd9a
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-11-05 23:54:23 +0700

    import jwt servivce to auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-    constructor(private userService: UsersService) { }
+    constructor(private userService: UsersService, private jwtService: JwtService) { }
 
     async validateUser(username: string, password: string): Promise<any> {
         const user = await this.userService.findOne(username);
@@ -14,5 +15,16 @@ export class AuthService {
         }
 
         return null;
+    }
+
+    async login(user: any) {
+        const payload = { name: user.name, sub: user.id }
+
+        return {
+            access_token: this.jwtService.sign(payload, {
+                secret: "Kopihitam645_",
+                expiresIn: "60s"
+            }),
+        }
     }
 }
